1a195ffb94c5968af8a3ea9e6faef1e689abbc30
salekhya <bonda.saalekhya@capgemini.com>
2023-11-29 09:12:33 +0530

    base-domains

---
+++ base-domains/src/main/java/net/javaguides/basedomains/dto/OrderEvent.java
@@ -0,0 +1,44 @@
+package net.javaguides.basedomains.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+
+
+    public OrderEvent() {}
+    public OrderEvent(String message, String status, Order order) {
+		super();
+		this.message = message;
+		this.status = status;
+		this.order = order;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+    
+}
